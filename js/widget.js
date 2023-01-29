eab71401fd7fec266ccd2b3d709581129ded81fd
onlyonemango <edmango001@gmail.com>
2023-01-30 00:15:36 +0900

    api연동완료

--- js/widget.js
+++ js/widget.js
@@ -37,33 +37,77 @@ $.getJSON(
       }
     }, []);
     console.log(filteredDay);
+
+    let date = "";
+    let dateList = [];
     $.each(filteredDay, function (i) {
-      let date = filteredDay[i].dt_txt.substr(0, 8); // 각 날짜(년-월-일)
+      date = filteredDay[i].dt_txt.substr(0, 10); // 각 날짜(년-월-일)
+
+      dateList.push(date);
 
       let day = filteredDay[i].dt_txt.substr(8, 2); //각 날짜의 일
       let trHTML = `
       <li class="swiper-slide">
-              <a href="">
-                  <p class="date">${day}</p>
-                  <p class="day">요일</p>
-              </a>
-          </li>
+      <a href="">
+      <p class="date">${day}</p>
+      <p class="day">요일</p>
+      </a>
+      </li>
       `;
       target.append(trHTML);
     }); //요일 리스트 가져오기
-
-    let maxTempArr = [];
-    let date = filteredDay[0].dt_txt.substr(8, 2);
+    console.log(dateList);
+    // let date = filteredDay[0].dt_txt.substr(8, 2);
     // console.log(date);
-    $.each(list, function (i) {
-      var maxTemp = list.filter(function (item) {
-        return item.dt_txt.substr(0, 10) == today;
+    let Temp = "";
+    $.each(dateList, function (i) {
+      Temp = list.filter(function (item) {
+        return item.dt_txt.substr(0, 10) == dateList[i];
       });
+      console.log(Temp);
+      let TempArr = [];
+      let codeArr = [];
+      let codeDescArr = [];
+      Temp.map((item) => {
+        TempArr.push(item.main.temp);
+        codeArr.push(item.weather[0].icon);
+        codeDescArr.push(item.weather[0].main);
+      });
+      console.log(TempArr);
+      console.log(codeArr);
+
+      let iconImg = `<img src="http://openweathermap.org/img/wn/${codeArr[i]}.png" alt="${codeDescArr[i]}">`;
+
+      let maxTemp = Math.max(...TempArr);
+      let minTemp = Math.min(...TempArr);
+
       console.log(maxTemp);
-      let todayMaxTemp = maxTemp[i].main.temp_max;
-      maxTempArr.push(todayMaxTemp);
-      console.log(maxTempArr); //오늘 날짜 최고온도추출
-      console.log(Math.max(maxTempArr[i]));
+      console.log(minTemp);
+      // console.log(todayTemp); //오늘 날짜 최고온도추출
+      // let maxTemp = Math.max(...todayTemp);
+      // let minTemp = Math.min(...todayTemp);
+      // console.log(maxTemp); //오늘 날짜 최고온도추출
+      // console.log(minTemp); //오늘 날짜 최고온도추출
+      // console.log(todayMaxTemp.sort().reverse());
+      trHTML = `
+      <li class="d-flex">
+      ${iconImg}
+        <span class="temp">
+          <p>MAX ${maxTemp}</p>
+          <p>MIN ${minTemp}</p>
+        <span>
+      </li>
+      `;
+      $(".preview ul").append(trHTML);
+    });
+
+    $(".preview ul li").eq(0).find("> *").show();
+    $(".swiper-slide a").click(function (e) {
+      e.preventDefault();
+      $(".preview ul li > *").hide();
+      let idx = $(this).parent().index();
+      console.log(idx);
+      $(".preview ul li").eq(idx).find("> *").show();
     });
   }
 );
