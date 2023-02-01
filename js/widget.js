e4a0bf7dba8a6557a79e6f0136a9fed3a742ca83
yedarm0101 <yedarm0101@gmail.com>
2023-02-01 18:22:40 +0900

    api수정/hover이벤트

--- js/widget.js
+++ js/widget.js
@@ -15,8 +15,30 @@ let year = newDate.getFullYear(); // 년도
 let month = newDate.getMonth() + 1; // 월
 let date = newDate.getDate(); // 날짜
 let day = newDate.getDay(); // 요일
+let korWeek = new Array("일", "월", "화", "수", "목", "금", "토");
 let today = `${year}-0${month}-${date}`;
-console.log(today);
+console.log(date);
+
+let weatherIcon = {
+  "01d": "fa-sun",
+  "01n": "fa-moon",
+  "02d": "fa-cloud-sun",
+  "02n": "fa-cloud-moon",
+  "03d": "fa-cloud",
+  "03n": "fa-cloud",
+  "04d": "fa-cloud",
+  "04n": "fa-cloud",
+  "09d": "fa-cloud-showers-heavy",
+  "09n": "fa-cloud-showers-heavy",
+  "10d": "fa-cloud-sun-rain",
+  "10n": "fa-cloud-moon-rain",
+  "11d": "fa-cloud-bolt",
+  "11n": "fa-cloud-bolt",
+  "13d": "fa-snowflake",
+  "13n": "fa-snowflake",
+  "50d": "fa-cloud-fog",
+  "50n": "fa-cloud-fog",
+};
 
 $.getJSON(
   "http://api.openweathermap.org/data/2.5/forecast?lat=37.5683&lon=126.9778&appid=6683b822924895deb0d2f90cf228a02e&units=metric",
@@ -24,7 +46,12 @@ $.getJSON(
     console.log(result);
     //console.log(result.list[0].dt);
     let list = result.list;
+
     console.log(list);
+    $.each(list, function (i) {
+      let convertDate = new Date(list[i].dt * 1000);
+      console.log(convertDate);
+    });
 
     const filteredDay = list.reduce((acc, current) => {
       const x = acc.find(
@@ -46,8 +73,7 @@ $.getJSON(
       dateList.push(date);
 
       let day = filteredDay[i].dt_txt.substr(8, 2); //각 날짜의 일
-      let korWeek = new Array("월", "화", "수", "목", "금", "토", "일");
-      let korDay = korWeek[new Date(dateList[i]).getDay() - 1];
+      let korDay = korWeek[new Date(dateList[i]).getDay()];
 
       let trHTML = `
       <li class="swiper-slide">
@@ -81,27 +107,6 @@ $.getJSON(
       console.log(TempArr);
       console.log(iconArr);
 
-      let weatherIcon = {
-        "01d": "fa-sun",
-        "01n": "fa-moon",
-        "02d": "fa-cloud-sun",
-        "02n": "fa-cloud-moon",
-        "03d": "fa-cloud",
-        "03n": "fa-cloud",
-        "04d": "fa-clouds",
-        "04n": "fa-clouds",
-        "09d": "fa-cloud-showers-heavy",
-        "09n": "fa-cloud-showers-heavy",
-        "10d": "fa-cloud-sun-rain",
-        "10n": "fa-cloud-moon-rain",
-        "11d": "fa-cloud-bolt",
-        "11n": "fa-cloud-bolt",
-        "13d": "fa-snowflake",
-        "13n": "fa-snowflake",
-        "50d": "fa-cloud-fog",
-        "50n": "fa-cloud-fog",
-      };
-
       let icon = `<i class="fa-solid ${weatherIcon[iconArr]}"></i>`;
       // console.log(weatherIcon[iconArr[i]]);
       let maxTemp = Math.max(...TempArr);
@@ -114,8 +119,8 @@ $.getJSON(
       <li class="d-flex gap-3">
       ${icon}
         <span class="temp">
-          <p>MAX ${maxTemp}</p>
-          <p>MIN ${minTemp}</p>
+          <p><span>MAX </span>${maxTemp.toFixed(1)}<span>˚C</span></p>
+          <p><span>MIN </span>${minTemp.toFixed(1)}<span>˚C</span></p>
         <span>
       </li>
       `;
@@ -129,14 +134,35 @@ $.getJSON(
       let idx = $(this).parent().index();
       $(".preview ul li").eq(idx).find("> *").show();
     });
+
+    //preview - icon
+    let previewIcon = list[0].weather[0].icon;
+    let previewIconBox = $(".today_icon");
+    let previewIconHTML = `<i class="fa-solid ${weatherIcon[previewIcon]}"></i>`;
+    previewIconBox.append(previewIconHTML);
+
+    //preview - temp
+    let todayTemp = $(".preview ul >li:first-child .temp").html();
+    let previewTemp = `<div class="d-flex justify-content-center">${todayTemp}</div>`;
+    let previewTempBox = $(".today_temp");
+
+    previewTempBox.html(previewTemp);
+    console.log(previewTemp);
+
+    //preview - date
+    let previewDateBox = $(".today_date");
+    let previewDateHTML = `<div class="d-flex justify-content-center "><p>0${newDate.getDate()}</p><p>(${
+      korWeek[day]
+    })</p></div>`;
+    previewDateBox.html(previewDateHTML);
   }
 );
 
-$(".weather_cast").hover(
-  function () {
-    $(this).addClass("active");
-  },
-  function () {
-    $(this).removeClass("active");
-  }
-);
+$(".box").mouseenter(function () {
+  $(this).addClass("active");
+  $(this).prev().addClass("active");
+});
+$(".weather_cast").mouseout(function () {
+  $(this).removeClass("active");
+  $(this).next().removeClass("active");
+});
