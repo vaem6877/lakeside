e9a1831c4e89cd543703cd1c4858ca7e12422f8a
unizazang <zifnffk32123@gmail.com>
2023-02-02 18:24:35 +0900

    notice

---
+++ js/notice.js
@@ -0,0 +1,7 @@
+// 시설안내
+
+let facWrap = $(".facility-wrapper"),
+  navBtn = facWrap.find(".facility-nav-wrapper ul li"),
+  navImg = facWrap.find(".fac-nav-img-wrapper img"),
+  facInfo = facWrap.find(".facility-information .facility-info-wrapper"),
+  pager = facWrap.find(".facility-nav-wrapper .fac-pager");
