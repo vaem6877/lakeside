7707905a09f061c712d82ad7f6c963969a28086c
unizazang <zifnffk32123@gmail.com>
2023-02-03 00:00:08 +0900

    notice

--- js/notice.js
+++ js/notice.js
@@ -4,4 +4,24 @@ let facWrap = $(".facility-wrapper"),
   navBtn = facWrap.find(".facility-nav-wrapper ul li"),
   navImg = facWrap.find(".fac-nav-img-wrapper img"),
   facInfo = facWrap.find(".facility-information .facility-info-wrapper"),
-  pager = facWrap.find(".facility-nav-wrapper .fac-pager");
+  pager = facWrap.find(".facility-nav-wrapper .fac-pager a");
+
+navBtn.click(function (e) {
+  e.preventDefault();
+  let tabIdx = $(this).index();
+  // navImg.removeClass("active");
+  // facInfo.removeClass("active");
+  // pager.removeClass("active");
+
+  // navImg.eq(tabIdx).addClass("active");
+  // facInfo.eq(tabIdx).addClass("active");
+  // pager.eq(tabIdx).addClass("active");
+
+  navImg.hide();
+  facInfo.hide();
+  pager.hide();
+
+  navImg.eq(tabIdx).fadeIn();
+  facInfo.eq(tabIdx).fadeIn();
+  pager.eq(tabIdx).fadeIn();
+}); //nav a 클릭시 할일
