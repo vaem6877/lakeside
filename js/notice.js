395e5677491d71679428798251d4f4f240c6aaaf
unizazang <zifnffk32123@gmail.com>
2023-02-04 01:36:30 +0900

    20230203

--- js/notice.js
+++ js/notice.js
@@ -33,10 +33,54 @@ function showSlide(idx) {
 
 // ======== aside nav ==============================================
 
-let asideNavWrapper = $(".aside-nav"),
+let wholeSectionWrap = $("main"),
+  asideNavWrapper = $(".aside-nav"),
   asideNav = asideNavWrapper.find("a"),
-  sectionOST;
+  sectionsOST = [],
+  currentIdx = 0;
 
 asideNav.each(function () {
   let sectionLink = $(this).attr("href");
+  sectionsOST.push($(sectionLink).offset().top);
 });
+
+wholeSectionWrap.on("mousewheel", function (event) {
+  let executed = false;
+  if (executed == false) {
+    moveSection(event.deltaY);
+  }
+});
+
+// asideNav.click(function () {});
+
+// ==== function ====
+function debounce(callback, time) {
+  let executed = false;
+  return () => {
+    if (!executed) {
+      callback();
+      executed = true;
+
+      setTimeout(() => {
+        executed = false;
+      }, time);
+    }
+  };
+}
+
+function moveSection(num) {
+  let nextNum = 0;
+
+  if (num == -1 && currentIdx < sectionsOST.length) {
+    nextNum = currentIdx + 1;
+  } else if (num == 1 && currentIdx > 0) {
+    nextNum = currentIdx - 1;
+  }
+
+  $("body, html")
+    .stop()
+    .animate({ scrollTop: sectionsOST[nextNum] }, 300, "easeInOutCirc"); //이 숫자번째만큼 스크롤을 만들어서 넘겨줘
+  // jquery ui 아직 로드 안함
+
+  currentIdx = nextNum;
+}
