0921ac1d34ca4bfe1c1d0a667dbd5a655e97dffa
unizazang <zifnffk32123@gmail.com>
2023-02-04 20:39:46 +0900

    aside nav bar js 추가

--- js/script.js
+++ js/script.js
@@ -18,6 +18,59 @@ topBtn.on("click", function (e) {
 
 // 탑버튼 끝 // 윤희 0202 ===========
 
+// aside nav bar 시작 ================================================
+
+let wholeSectionWrap = $("main"),
+  asideNavWrapper = $(".aside-nav"),
+  asideNav = asideNavWrapper.find("a"),
+  sections = [],
+  sectionsOST = [],
+  currentIdx = 0,
+  targetIdx;
+
+asideNav.each(function () {
+  let sectionLink = $(this).attr("href");
+  sections.push($(sectionLink));
+  sectionsOST.push($(sectionLink).offset().top);
+});
+
+asideNav.click(function (e) {
+  e.preventDefault();
+
+  targetIdx = $(this).index();
+
+  $("html, body")
+    .stop()
+    .animate({
+      scrollTop: sectionsOST[targetIdx] - 200,
+    });
+});
+
+// 스크롤양에 따라 보이고 안보임
+function btnFade(btn, ost) {
+  let scrAmt = $(window).scrollTop();
+
+  if (scrAmt > ost) {
+    btn.fadeIn();
+  } else {
+    btn.fadeOut();
+  }
+}
+
+$(window).scroll(function () {
+  let SCT = $(this).scrollTop();
+  btnFade(asideNav, 830);
+
+  $.each(sections, function (idx, item) {
+    if (SCT >= item.offset().top - 400) {
+      asideNav.removeClass("active");
+      asideNav.eq(idx).addClass("active");
+    }
+  });
+});
+
+// ========================= aside bar 끝 // 0204 윤희
+
 //메뉴 시작 ======================================================
 
 let menu = $(".menu"),
