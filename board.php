bae6ead2b6d550ece84a1a4ad0131f549bd6f1d9
vaem6877 <vaem6877@naver.com>
2023-02-05 23:37:41 +0900

    백단 디자인 수정

--- board.php
+++ board.php
@@ -9,10 +9,14 @@ require_once('php/config.php');
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>lakeside - board</title>
+    <title>lakeside - book</title>
     <link
       rel="stylesheet"
       href="	https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css"
+    />
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css"
     />
     <link
       rel="stylesheet"
@@ -27,10 +31,95 @@ require_once('php/config.php');
   <body>
     <header class="text-center board_header">
       <h1 id="logo"><a href="#">lakeside</a></h1>
-      <div class="widget"></div>
+      <div class="widget_box d-flex">
+        <div class="weather_cast">
+          <div class="preview d-flex">
+            <ul class="d-flex"></ul>
+          </div>
+          <div class="swiper container">
+            <ul class="swiper-wrapper"></ul>
+          </div>
+          <div class="swiper-button-prev">◀</div>
+          <div class="swiper-button-next">▶</div>
+          <div class="shortcut">
+            <div
+              class="reservation d-flex justify-content-center align-items-center gap-4"
+            >
+              <div class="calendar"><p>예약하기</p></div>
+              <i class="fa-solid fa-calendar-plus"></i>
+            </div>
+            <div
+              class="faq d-flex justify-content-center align-items-center gap-4"
+            >
+              <div class="phone"><p>문의하기</p></div>
+              <i class="fa-solid fa-square-phone-flip"></i>
+            </div>
+            <div
+              class="location d-flex justify-content-center align-items-center gap-4"
+            >
+              <div class="map"><p>오시는길</p></div>
+              <i class="fa-solid fa-map-location-dot"></i>
+            </div>
+          </div>
+        </div>
+        <div class="preview_widget">
+          <div class="today_weather">
+            <div class="today_icon"></div>
+            <div class="today_temp"></div>
+            <div class="today_date"></div>
+            <div class="preview_shortcut">
+              <p>
+                <i class="fa-solid fa-calendar-plus"></i>
+              </p>
+              <p><i class="fa-solid fa-square-phone-flip"></i></p>
+              <p><i class="fa-solid fa-map-location-dot"></i></p>
+            </div>
+          </div>
+        </div>
+      </div>
       <!-- <div class="icon_mouse">
         <img src="img/common/icon_mouse.svg" alt="" />
       </div> -->
+      <div
+        class="menu d-flex flex-column justify-content-center align-items-center"
+      >
+        <ul class="main_menus d-flex">
+          <li><a href="">예약하기</a></li>
+          <li><a href="">코스안내</a></li>
+          <li><a href="">클럽안내</a></li>
+          <li><a href="">클럽인포</a></li>
+          <li><a href="">회원정보</a></li>
+        </ul>
+        <div class="sub_menus d-flex">
+          <ul class="sub_menu">
+            <li><a href="">예약안내</a></li>
+            <li><a href="">예약하기</a></li>
+            <li><a href="">예약확인</a></li>
+          </ul>
+          <ul class="sub_menu">
+            <li><a href="">코스소개</a></li>
+            <li><a href="">코스갤러리</a></li>
+            <li><a href="">홀인원이벤트</a></li>
+          </ul>
+          <ul class="sub_menu">
+            <li><a href="">연혁/대회</a></li>
+            <li><a href="">이용안내</a></li>
+            <li><a href="">시설안내</a></li>
+            <li><a href="">오시는길</a></li>
+          </ul>
+          <ul class="sub_menu">
+            <li><a href="">공지사항</a></li>
+            <li><a href="">명예의 전당</a></li>
+          </ul>
+          <ul class="sub_menu">
+            <li><a href="">회원정보관리</a></li>
+            <li><a href="">쿠폰함</a></li>
+          </ul>
+        </div>
+        <div class="button">
+          <p>=</p>
+        </div>
+      </div>
     </header>
     <main class="board_wrapper">
       <div class="container">
@@ -98,6 +187,10 @@ require_once('php/config.php');
         </div>
       </div>
     </main>
+ 
+    <a href="#" id="go-top"><i class="fa-solid fa-arrow-up"></i></a>
+    <!-- 탑 버튼 -->
+
     <footer>
       <div class="terms d-flex justify-content-center">
         <a href="">이용약관</a>
@@ -137,5 +230,20 @@ require_once('php/config.php');
         </p>
       </address>
     </footer>
+    <script
+      src="https://code.jquery.com/jquery-3.6.3.min.js"
+      integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU="
+      crossorigin="anonymous"
+    ></script>
+    <script
+      src="https://code.jquery.com/ui/1.13.2/jquery-ui.min.js"
+      integrity="sha256-lSjKY0/srUM9BE3dPm+c4fBo1dky2v27Gdjm2uoZaL0="
+      crossorigin="anonymous"
+    ></script>
+    <script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
+    <script src="js/back.js"></script>
+    <script src="js/script.js"></script>
+    <script src="js/widget.js"></script>
   </body>
 </html>
+
