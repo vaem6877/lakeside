df3aeefc216ed002273fca593566f983df3070f1
vaem6877 <vaem6877@naver.com>
2023-02-06 11:05:39 +0900

    구로드물 제거

--- session.php
+++ session.php
@@ -220,6 +220,5 @@
     <script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
     <script src="js/back.js"></script>
     <script src="js/script.js"></script>
-    <script src="js/widget.js"></script>
   </body>
 </html>
