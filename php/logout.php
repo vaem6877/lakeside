9dcd2b63b0391e90760f022609430f49bb18fef5
vaem6877 <vaem6877@naver.com>
2023-02-06 17:55:13 +0900

    자기 브랜치 정리

--- php/logout.php
+++ php/logout.php
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 <?php
     session_start();
     session_destroy();
     ?>
     <script>
         location.replace('../session.php')
-=======
-<?php
-    session_start();
-    session_destroy();
-    ?>
-    <script>
-        location.replace('../session.php')
->>>>>>> 8ee7bd5a7971d00b8f306d8a4fd64c3896dcf805
         </script>
