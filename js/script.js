8b9385b20a04c872e65043bd3ee938b44a17ff76
unizazang <zifnffk32123@gmail.com>
2023-02-07 12:50:37 +0900

    common.js nav bar 오류 수정

--- js/script.js
+++ js/script.js
@@ -67,7 +67,6 @@ $(window).scroll(function () {
     if (SCT >= item.offset().top - 400) {
       asideNav.removeClass("active");
       asideNav.eq(idx).addClass("active");
-      asideNav.css({ display: "inline-block" });
     }
   });
 });
